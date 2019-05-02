278fdc312ce2abca8e53ce3ba1049534395016ac
Robert Novotny <novotnr0@gmail.com>
2019-05-02 23:37:40 +0200

    Add skeleton Fotoapparat view and variables

--- app/src/main/java/com/github/novotnyr/android/fotic/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/fotic/MainActivity.java
@@ -1,14 +1,22 @@
 package com.github.novotnyr.android.fotic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.fotoapparat.view.CameraView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.cameraView = findViewById(R.id.cameraView);
+    }
+
+    public void onSnapButtonClick(View view) {
     }
 }
